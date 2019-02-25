7a3c941b7775b9547a392bde7c372c98ead39c7b
MikeForMH <michaelhuixu@gmail.com>
2019-02-25 19:16:20 +0800

    Initial Commit

---
+++ my_c_practice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  my_c_practice
+//
+//  Created by Michael Hui on 25/2/2019.
+//  Copyright © 2019 Michael Hui. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
